fecfe5cd460261667f67f709a8da42ca25b1e6d6
Abhishek jat <abhishekjat80094@gmail.com>
2025-02-24 16:04:01 +0530

    [Abhishek Jat] Add. Ability for the Greeting App to save the Greeting Message in the Repository

--- src/main/java/com/demo/mygreetingapp/controller/GreetingController.java
+++ src/main/java/com/demo/mygreetingapp/controller/GreetingController.java
@@ -1,32 +1,32 @@
 package com.demo.mygreetingapp.controller;
 
+import com.demo.mygreetingapp.entity.GreetingMessage;
+import com.demo.mygreetingapp.service.GreetingService;
 import com.demo.mygreetingapp.model.GreetingModel;
 import com.demo.mygreetingapp.model.GreetingRequest;
-import com.demo.mygreetingapp.service.GreetingService;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 // Base URL for all endpoints
 @RestController
 @RequestMapping("/greet")
 public class GreetingController {
-
 	private final GreetingService greetingService;
 
 	public GreetingController(GreetingService greetingService) {
 		this.greetingService = greetingService;
 	}
 
-	// Handle GET requests (Default Greeting)
-	@GetMapping
-	public GreetingModel getDefaultGreeting() {
-		return new GreetingModel(greetingService.getGreetingMessage(null, null));
-	}
-
-	// Handle POST requests (Personalized Greeting)
+	// Create and Save Greeting
 	@PostMapping
 	public GreetingModel createGreeting(@RequestBody GreetingRequest request) {
-		String message = greetingService.getGreetingMessage(request.getFirstName(), request.getLastName());
-		return new GreetingModel(message);
+		return greetingService.generateAndSaveGreeting(request);
+	}
+
+	// Fetch All Saved Greetings
+	@GetMapping("/all")
+	public List<GreetingMessage> getAllGreetings() {
+		return greetingService.getAllGreetings();
 	}
 
 	// Handles PUT requests
